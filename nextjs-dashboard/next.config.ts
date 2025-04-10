c14357a8292b019fef6d89e0c688f1884040ca64
Daniel Santiago Sandoval Higuera <95585487+hisandan@users.noreply.github.com>
2025-04-10 01:55:13 +0000

    Allows Codespace Origin when calling  Next Actions

--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -1,10 +1,32 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   experimental: {
-    ppr: 'incremental'
+    ppr: 'incremental',
+    serverActions: {
+      allowedOrigins: ["ideal-space-winner-gvqx7q6wqxw29gwj-3000.app.github.dev", "localhost:3000"]
+    }
   }
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig
+
+// import type { NextConfig } from 'next';
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   experimental: {
+//     ppr: 'incremental',
+//     serverActions: {
+//       // edit: updated to new key. Was previously `allowedForwardedHosts`
+//       allowedOrigins: ['ideal-space-winner-gvqx7q6wqxw29gwj-3000.app.github.dev'],
+//       // allowedForwardedHosts :  ['ideal-space-winner-gvqx7q6wqxw29gwj-3000.app.github.dev'],
+//     },
+//   }
+// };
+
+
+
+// export default nextConfig;
+
+
+
