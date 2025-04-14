89f957004a72cb1a3eb3b27f9d7872baa351cdf1
Turbobot <turbobot@vercel.com>
2025-04-14 23:58:18 +0000

    feat(create-turbo): apply official-starter transform

--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -3,30 +3,13 @@ const nextConfig = {
   experimental: {
     ppr: 'incremental',
     serverActions: {
-      allowedOrigins: ["ideal-space-winner-gvqx7q6wqxw29gwj-3000.app.github.dev", "localhost:3000"]
+      allowedOrigins: ["ideal-space-winner-gvqx7q6wqxw29gwj-3000.app.github.dev", "localhost:3000", process.env.NEXT_PUBLIC_BASE_URL]
     }
   }
 }
 
 module.exports = nextConfig
 
-// import type { NextConfig } from 'next';
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   experimental: {
-//     ppr: 'incremental',
-//     serverActions: {
-//       // edit: updated to new key. Was previously `allowedForwardedHosts`
-//       allowedOrigins: ['ideal-space-winner-gvqx7q6wqxw29gwj-3000.app.github.dev'],
-//       // allowedForwardedHosts :  ['ideal-space-winner-gvqx7q6wqxw29gwj-3000.app.github.dev'],
-//     },
-//   }
-// };
-
-
-
-// export default nextConfig;
 
 
 
